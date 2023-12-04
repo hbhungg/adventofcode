1e4acb9365343776e9a033a2594892b4b3d109c3
hung <bahung1tt@gmail.com>
2023-12-04 22:09:14 +1100

    cleanup

--- 2023/4/solve.exs
+++ 2023/4/solve.exs
@@ -17,7 +17,7 @@ p1 = data
   |> Enum.map(fn x -> if x > 0, do: 2**(x-1), else: x end)
   |> Enum.sum()
 
-p2 = data
+rep = data
   |> Enum.with_index
   |> Enum.map(fn {k,v} -> {v+1, (if k>0, do: Enum.to_list(v+2..v+k+1), else: [])} end) |> Map.new
 
@@ -32,11 +32,11 @@ defmodule SpawnCard do
 end
 
 # IO.inspect data
-# IO.inspect p1
-# IO.inspect p2
 
-v = 1..map_size(p2)
-  |> Enum.map(&SpawnCard.winr(&1, p2, map_size(p2)))
+p2 = 1..map_size(rep)
+  |> Enum.map(&SpawnCard.winr(&1, rep, map_size(rep)))
   |> Enum.sum()
-IO.inspect v+map_size(p2)
+  |> Kernel.+(map_size(rep))
 
+IO.inspect p1
+IO.inspect p2
